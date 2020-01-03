a2aaa06d5353b287c3be75dbaa28c0294d20b319
Johnny Nie <jiayi@Johnny.local>
2020-01-03 18:48:30 +0800

    Initial commit

---
+++ src/main/java/com/jiayi/ws/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.jiayi.ws.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@Slf4j
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        log.info("======configureMessageBrokers");
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        log.info("======注册registerStompEndpoints");
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
+
+}
---
+++ src/main/java/com/jiayi/ws/model/Greeting.java
@@ -0,0 +1,12 @@
+package com.jiayi.ws.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+    private String content;
+}
